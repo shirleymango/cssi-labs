4792f2f39c80555b1902cd37b33d10f84c4bd969
Billy Hardy <billy92@gmail.com>
2021-07-28 14:08:16 +0000

    Add shell scripts that will copy firebase init files from day11 starter to final and day12 starter, as well as day12 starter to final

---
+++ update-to-day12.sh
@@ -0,0 +1,3 @@
+cp day11/starter/firebaseNotes/firebase.json day12/starter/firebaseNotes/firebase.json
+cp day11/starter/firebaseNotes/database.rules.json day12/starter/firebaseNotes/database.rules.json
+cp day11/starter/firebaseNotes/.firebaserc day12/starter/firebaseNotes/.firebaserc
